19cbb810834671892dae2266c145594934f1597c
Edison Estival Restrepo Ospina <edisonestival@gmail.com>
2022-12-27 11:10:09 -0500

    Challenge One - Alura

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# Encriptador-de-texto
-Challenge ONE Encriptador de texto
+# Challenge ONE - Alura
+
+## Encriptador de texto con Javascript
+
+
+## Edison Estival Restrepo Ospina 
+
+## **Enlace a la página en vivo:** [encriptador-de-texto.com](https://edirestrepo.github.io/Encriptador-de-texto/ "Encriptador-de-texto") 
+
+👌💻
---
+++ js/main.js
@@ -0,0 +1,58 @@
+function encrypted(){
+
+    var text = document.getElementById("text-encryted").value.toLowerCase();
+
+    /**
+     * i = es para que tome letras mayusculas y minusculas
+     *  m = tome en cuenta toda la linea ó la oración
+     * g = tome en cuenta multiples lineas
+     * 
+     */ 
+    var textEncrypted = text.replace(/e/img, "enter");
+    var textEncrypted = textEncrypted.replace(/o/img, "ober");
+    var textEncrypted = textEncrypted.replace(/i/img, "imes");
+    var textEncrypted = textEncrypted.replace(/a/img, "ai");
+    var textEncrypted = textEncrypted.replace(/u/img, "ufat");
+
+    document.getElementById("text-unencrypted").innerHTML = textEncrypted;
+    document.getElementById("hide-image").style.display = "none";
+    document.getElementById("button-copy").style.display = "show";
+    document.getElementById("button-copy").style.display = "inherit";
+    document.getElementById("text-unencrypted").style.display = "show";
+    document.getElementById("text-unencrypted").style.display = "inherit";
+}
+
+function unencrypted(){
+
+    var text = document.getElementById("text-encryted").value.toLowerCase();
+
+    var textEncrypted = text.replace(/enter/img, "e");
+    var textEncrypted = textEncrypted.replace(/ober/img, "o");
+    var textEncrypted = textEncrypted.replace(/imes/img, "i");
+    var textEncrypted = textEncrypted.replace(/ai/img, "a");
+    var textEncrypted = textEncrypted.replace(/ufat/img, "u");
+
+    document.getElementById("text-unencrypted").innerHTML = textEncrypted;
+    document.getElementById("hide-image").style.display = "none";
+    document.getElementById("button-copy").style.display = "show";
+    document.getElementById("button-copy").style.display = "inherit";
+    document.getElementById("text-unencrypted").style.display = "show";
+    document.getElementById("text-unencrypted").style.display = "inherit";
+}
+
+function copy(){
+
+    var content = document.querySelector("#text-unencrypted");
+
+    content.select();
+    document.execCommand("copy");
+}
+
+var btn = document.querySelector("#button-encrypted");
+btn.onclick = encrypted;
+
+var btn2 = document.querySelector("#button-unencrypted");
+btn2.onclick = unencrypted;
+
+var btn3 = document.querySelector("#button-copy");
+btn3.onclick = copy;
